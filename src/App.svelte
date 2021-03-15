9a9a5feb22675a28e5954a09d8b86c46e82062a9
Anthony Sylvester <anthony.sylvester99@gmail.com>
2021-03-15 02:09:22 -0700

    refactors css

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
-  import { onMount, afterUpdate, tick } from 'svelte'
+  import { onMount, afterUpdate } from 'svelte'
   import {
     intervalToDuration,
     differenceInDays,
     differenceInMilliseconds,
   } from 'date-fns'
   export const message = 'default'
-
-  const toGoRaw = intervalToDuration({
-    start: Date.now(),
-    end: new Date(2021, 5, 11, 18),
-  })
 
   let days = 0,
     hours = 0,
@@ -33,64 +28,34 @@
   }
   requestAnimationFrame(timeLoop)
 
-  onMount(async () => {
-    let hero = document.getElementById('hero')
-    let logo = document.getElementById('logo')
-    let countdown = document.getElementById('countdown')
-
-    window.addEventListener('resize', (e) => {
-      let hero = document.getElementById('hero')
-      const logoOffset = hero.offsetTop - 1 + 'px'
-      logo.style.top = logoOffset
-      countdown.style.bottom = logoOffset
-    })
-
-    hero.addEventListener('load', (e) => {
-      const logoOffset = (e.target as HTMLElement).offsetTop - 1 + 'px'
-      logo.style.top = logoOffset
-      countdown.style.bottom = logoOffset
-    })
-  })
+  onMount(async () => {})
 
   afterUpdate(async () => {})
 </script>
 
 <main>
-  <div class="container">
-    <img id="logo" src="/logo.webp" alt="The Guilty Gear Strive Logo" />
-    <img
-      id="hero"
-      src="/strive.webp"
-      alt="Sol and Ky Looking all Myspacey and deep."
-    />
+  <img id="logo" src="/logo.webp" alt="The Guilty Gear Strive Logo" />
+  <img
+    id="hero"
+    src="/strive.webp"
+    alt="Sol and Ky Looking all Myspacey and deep."
+  />
 
-    <div id="countdown">
-      <h1>
-        {days} <span>d</span>ays {hours} <span>h</span>ours
-      </h1>
-      <h1>
-        {mins} <span>m</span>ins {secs} <span>s</span>ecs
-      </h1>
-      <h1 id="milliseconds">{ms}</h1>
-    </div>
+  <div id="countdown">
+    <h1>
+      {days} <span>d</span>ays {hours} <span>h</span>ours
+    </h1>
+    <h1>
+      {mins} <span>m</span>ins {secs} <span>s</span>ecs
+    </h1>
+    <h1 id="milliseconds">{ms}</h1>
   </div>
 </main>
 
 <style>
   main {
-    background-color: black;
     height: 100%;
-    display: flex;
-    justify-content: center;
-  }
-
-  .container {
-    position: relative;
-    max-height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    overflow: hidden;
   }
 
   #logo {
@@ -111,9 +76,8 @@
   #hero {
     background-color: #651216;
     width: 100%;
-    height: auto;
-    max-height: 100%;
-    object-fit: fill;
+    height: 100%;
+    object-fit: cover;
     object-position: right;
   }
 
@@ -122,7 +86,7 @@
     color: rgba(255, 255, 255, 0.925);
     text-transform: uppercase;
     font-family: 'RED';
-    font-size: calc(5vw);
+    font-size: calc(2rem + 2vw);
     font-weight: 100;
   }
 
@@ -137,17 +101,18 @@
     bottom: 0;
     background: linear-gradient(
       90deg,
-      rgba(252, 249, 249, 0.534) 0%,
+      rgb(26, 21, 21) 0%,
       rgba(0, 212, 255, 0) 100%
     );
   }
 
-  #milliseconds {
-  }
-
-  @media (min-width: 200px) {
+  @media (max-width: 700px) {
     #hero {
-      object-fit: contain;
+      object-position: 85% 50%;
+    }
+
+    h1 > span {
+      color: blue;
     }
   }
 </style>
